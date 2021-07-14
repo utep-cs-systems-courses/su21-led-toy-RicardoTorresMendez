237d92cca6502a5746f5a737834e94cd34615eba
RicardoTorresMendez <josertorresm1996@gmail.com>
2021-07-13 23:35:47 -0500

    test

--- project/wdInterruptHandler.c
+++ project/wdInterruptHandler.c
@@ -4,10 +4,10 @@
 void __interrupt_vec(WDT_VECTOR) WDT(){  /* 250 interrupts/sec */
   static char blink_count = 0;
 
+  if(blink_count % 1 == 0){ turn_green_on(); }
   
 
   if (++blink_count == 250){ 
-    turn_red_on();
     blink_count = 0;
   }
 
