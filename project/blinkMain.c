b8b448e3330e8c668cf0785f94d57f377d6683a5
RicardoTorresMendez <josertorresm1996@gmail.com>
2021-07-15 00:41:30 -0500

    button to red led test

--- project/blinkMain.c
+++ project/blinkMain.c
@@ -3,11 +3,14 @@
 #include "libTimer.h"
 #include "led.h"
 #include "switches.h"
+#include "buzzer.h"
 
 int main(void) {
   configureClocks();		/* setup master oscillator, CPU & peripheral clocks */
   led_init();
   switch_init();
+  buzzer_init();
+  buzzer_set_period(1000);	/* start buzzing!!! */
   enableWDTInterrupts();	/* enable periodic interrupt */
 
   or_sr(0x18);		/* CPU off, GIE on */
